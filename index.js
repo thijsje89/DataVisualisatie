f9e3158553da4e8ff8de85ea2fdaf9989348a2ab
thijsje89 <thijsdrykoningen@hotmail.nl>
2019-01-15 22:36:54 +0100

    Datavisualisatie V3
    Nog een text functie toegevoegd die labels weergeeft op de bars. Verder nog wat styling en documentatie gedaan.

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 (function Module() {
     //private variabels
-    var dataset = [50, 40, 70, 80, 30];
-    var svgWidth = 400,
+    var dataset = [30, 70, 50, 80];
+    var label = ["Den Bosch", "Amsterdam", "Eindhoven", "Utrecht", ];
+    
+    var svgWidth = 360,
         svgHeight = 300,
         barPadding = 10;
     var barWidth = (svgWidth / dataset.length);
-    var svg = d3.select('svg');
+    var svg = d3.select('svg')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
     var yScale = d3.scaleLinear();
     var barChart = svg.selectAll('rect');
     var text = svg.selectAll("text");
+    var centerbar = (barWidth / 2);
 
-   
+
     //verandert de grootte van de bars aan de hand van de bar met de grootste data
     function yScaleMethod() {
-        svg.attr("width", svgWidth) //maakt aan de hand van de opgegeven svgHeight en width het canvas aan waarin we de verschillende bars neerzetten
+        //maakt aan de hand van de opgegeven svgHeight en width het canvas aan waarin we de verschillende bars neerzetten
+        svg.attr("width", svgWidth)
             .attr("height", svgHeight);
+
         yScale.domain([0, d3.max(dataset)])
-            .range([0, svgHeight])
+            .range([0, svgHeight]);
     }
     //maakt aan de hand van de data de juiste hoeveelheid bars en met de juiste grootte
     function barChartMethod() {
-        svg.attr("width", svgWidth)
-            .attr("height", svgHeight);
+        svg
         barChart.data(dataset)
             .enter()
             .append("rect")
             .attr("fill", "#ffb000")
+            .attr("id", "bar")
             .attr("y", function (d) {
                 return svgHeight - yScale(d) //Hoe meer de y as is hoe lager het getal van de informatie is. Dus stel ik heb een dataset van 80, 50, 30, 70. Dan wordt er bij het getal 30 meer afgehaald om ervoor te zorgen dat deze op de goede plek staat. Dit is wat de ingebouwde scaleLinear functie doet. Die ik hier gebruik om de hoogte van de bars aan te passen aan de data.
 
@@ -39,14 +46,14 @@
                 var translate = [barWidth * i, 0];
                 return "translate(" + translate + ")";
             });
+            
     }
     // zet de juiste text op de goede plek neer die opgehaald wordt uit de dataset.
-    function textMethod() {
-        svg.attr("width", svgWidth)
-            .attr("height", svgHeight);
+    function nmbMethod() {
+       
         text.data(dataset)
             .enter()
-            .append("text") 
+            .append("text")
             .attr("class", "infotext")
             .attr("font-size", "30px")
             .text(function (d) { //pakt de data uit de dataset en stopt het in het text variabele
@@ -54,16 +61,40 @@
 
             })
             .attr("y", function (d, i) { //bepaalt de locatie van de tekst op de y as
-                return svgHeight; 
+                return svgHeight - yScale(d) + 40;
             })
+            .attr("x", function (d, i) { //bepaalt de locatie van de tekst op de x as
+                return barWidth * i + 19;
+            })
+            .attr("fill", "#ffffff") //kleur van de tekst
+    }
+    function textMethod() {
+        text.data(label)
+            .enter()
+            .append("text")
+            .attr("class", "infotext")
+            .attr("font-size", "13px")
+            
+            .text(function (d, i) { //pakt de data uit de dataset en stopt het in het text variabele
+                return label[i];
+
+            })
+            .attr("y", "300") 
             .attr("x", function (d, i) { //bepaalt de locatie van de tekst op de x as
                 return barWidth * i;
             })
             .attr("fill", "#ffffff") //kleur van de tekst
+
+          
     }
+    
+
     //hier voer ik alle methode functies uit die ik wil gebruiken: (alle methodes zijn apart van elkaar uit te voeren, modulair) 
     yScaleMethod();
     barChartMethod();
+    nmbMethod();
     textMethod();
+
+    
 
 })();
